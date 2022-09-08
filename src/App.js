bda03a1204fcbd06b2b72f856aebdf00df7a1ed7
sonu20161023 <sonukumar9631111009@gmail.com>
2022-09-08 14:42:30 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Temp from './Temp'
+import './style.css';
+
+const App = () => {
+  return (
+    <div>
+      <Temp/>
+    
+    </div>
+  )
+}
+
+export default App
